e852b34772d2b15d4fc5495a34f876f03fc0790f
CHA JEONGMIN <cjmin1233@gmail.com>
2025-11-12 11:34:19 +0900

    레이어 관리 로직 리팩토링
    - LayerStack 은 단순 자료구조이므로 OnDetach, OnAttach 를 호출하지 않도록 변경.
    - Application 같은 Push, Pop 하는 주체가 호출하도록 함.

--- Jade/src/Jade/Core/Application.cpp
+++ Jade/src/Jade/Core/Application.cpp
@@ -32,6 +32,11 @@ namespace Jade
 
     Application::~Application()
     {
+        for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
+        {
+            (*it)->OnDetach();
+        }
+
         Renderer::Shutdown();
     }
 
@@ -85,11 +90,13 @@ namespace Jade
     void Application::PushLayer(Layer* layer)
     {
         m_LayerStack.PushLayer(layer);
+        layer->OnAttach();
     }
 
     void Application::PushOverlay(Layer* layer)
     {
         m_LayerStack.PushOverlay(layer);
+        layer->OnAttach();
     }
 
     bool Application::OnWindowClose(WindowCloseEvent& e)
--- Jade/src/Jade/Core/LayerStack.cpp
+++ Jade/src/Jade/Core/LayerStack.cpp
@@ -14,7 +14,6 @@ namespace Jade
     {
         for (Layer* layer : m_Layers)
         {
-            layer->OnDetach();
             delete layer;
         }
     }
@@ -23,15 +22,11 @@ namespace Jade
     {
         m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
         m_LayerInsertIndex++;
-
-        layer->OnAttach();
     }
 
     void LayerStack::PushOverlay(Layer* overlay)
     {
         m_Layers.emplace_back(overlay);
-
-        overlay->OnAttach();
     }
 
     void LayerStack::PopLayer(Layer* layer)
@@ -40,7 +35,6 @@ namespace Jade
 
         if (it != m_Layers.begin() + m_LayerInsertIndex)
         {
-            layer->OnDetach();
             m_Layers.erase(it);
             m_LayerInsertIndex--;
         }
@@ -52,7 +46,6 @@ namespace Jade
 
         if (it != m_Layers.end())
         {
-            overlay->OnDetach();
             m_Layers.erase(it);
         }
     }
