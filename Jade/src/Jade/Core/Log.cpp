99ce8e7f0cf3f611e06e477399885a57fc26a3fd
CHA JEONGMIN <cjmin1233@gmail.com>
2025-11-14 10:21:09 +0900

    로깅 시스템 개선 및 파일 출력 추가
    - 콘솔(`stdout_color_sink_mt`)과 파일(`basic_file_sink_mt`)에 로그를 동시에 출력하도록 변경.
    - 로그 출력 패턴을 콘솔과 파일에 맞게 세분화.

--- Jade/src/Jade/Core/Log.cpp
+++ Jade/src/Jade/Core/Log.cpp
@@ -2,6 +2,7 @@
 
 #include "Jade/Core/Log.h"
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Jade
 {
@@ -10,13 +11,21 @@ namespace Jade
 
     void Log::Init()
     {
-        std::string pattern_string = "%^[%T] %n: %v%$";
-        spdlog::set_pattern(pattern_string);
+        std::vector<spdlog::sink_ptr> logSinks;
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Jade.log", true));
 
-        s_CoreLogger = spdlog::stdout_color_mt("JADE");
+        logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+        logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+        s_CoreLogger = std::make_shared<spdlog::logger>("JADE", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_CoreLogger);
         s_CoreLogger->set_level(spdlog::level::trace);
+        s_CoreLogger->flush_on(spdlog::level::trace);
 
-        s_ClientLogger = spdlog::stdout_color_mt("APP");
+        s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_ClientLogger);
         s_ClientLogger->set_level(spdlog::level::trace);
+        s_ClientLogger->flush_on(spdlog::level::trace);
     }
 } // namespace Jade
