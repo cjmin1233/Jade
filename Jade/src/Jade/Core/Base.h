68f9b7d3278bb820c57dffd65b4de129fbae71e4
CHA JEONGMIN <cjmin1233@gmail.com>
2025-11-26 10:25:03 +0900

    JADE_BIND_EVENT_FN 매크로 수정 - std::bind 대신 람다 함수를 사용하여 최적화.

--- Jade/src/Jade/Core/Base.h
+++ Jade/src/Jade/Core/Base.h
@@ -48,7 +48,8 @@
 #define FMT_UNICODE 0   // Disable unicode support in fmt library
 
 #define BIT(x) (1 << x)
-#define JADE_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+//#define JADE_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define JADE_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace Jade
 {
