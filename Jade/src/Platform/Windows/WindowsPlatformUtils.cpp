d205163da8dfb6f6743491c7bbd10c03de66fd69
CHA JEONGMIN <cjmin1233@gmail.com>
2025-12-10 21:37:50 +0900

    OpenFile, SaveFile -> working directory 에서 시작하도록 변경

--- Jade/src/Platform/Windows/WindowsPlatformUtils.cpp
+++ Jade/src/Platform/Windows/WindowsPlatformUtils.cpp
@@ -6,7 +6,7 @@
 #include <commdlg.h>
 #include <GLFW/glfw3.h>
 #ifndef GLFW_EXPOSE_NATIVE_WIN32
-    #define GLFW_EXPOSE_NATIVE_WIN32
+#define GLFW_EXPOSE_NATIVE_WIN32
 #endif
 #include <GLFW/glfw3native.h>
 
@@ -14,11 +14,13 @@ namespace Jade
 {
     std::optional<std::string> FileDialogs::OpenFile(const char* filter)
     {
-        OPENFILENAMEA ofn;  // common dialog box structure
-        CHAR szFile[260] = { 0 };   // buffer for file name
+        OPENFILENAMEA ofn;              // common dialog box structure
+        CHAR szFile[260] = { 0 };       // buffer for file name
+        CHAR currentDir[256] = { 0 };   // buffer for current directory
 
         ZeroMemory(&ofn, sizeof(ofn));
 
+        // Fill in the OPENFILENAME structure
         ofn.lStructSize = sizeof(ofn);  // size of structure
         ofn.hwndOwner = glfwGetWin32Window((GLFWwindow*)Application::Get().GetWindow().GetNativeWindow());  // owner window
         ofn.lpstrFilter = filter;   // filter string
@@ -27,6 +29,12 @@ namespace Jade
         ofn.nMaxFile = sizeof(szFile);  // maximum file name size
         ofn.lpstrTitle = "Open File";  // title of dialog
         ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;  // dialog options
+
+        // Get the current directory to set as the initial directory
+        if (GetCurrentDirectoryA(sizeof(currentDir), currentDir))
+        {
+            ofn.lpstrInitialDir = currentDir;
+        }
 
         // Display the Open dialog box
         if (GetOpenFileNameA(&ofn))
@@ -43,9 +51,11 @@ namespace Jade
     {
         OPENFILENAMEA ofn;  // common dialog box structure
         CHAR szFile[260] = { 0 };   // buffer for file name
+        CHAR currentDir[256] = { 0 };   // buffer for current directory
 
         ZeroMemory(&ofn, sizeof(ofn));
 
+        // Fill in the OPENFILENAME structure
         ofn.lStructSize = sizeof(ofn);  // size of structure
         ofn.hwndOwner = glfwGetWin32Window((GLFWwindow*)Application::Get().GetWindow().GetNativeWindow());  // owner window
         ofn.lpstrFilter = filter;   // filter string
@@ -53,10 +63,19 @@ namespace Jade
         ofn.lpstrFile = szFile; // buffer for file name
         ofn.nMaxFile = sizeof(szFile);  // maximum file name size
         ofn.lpstrTitle = "Save File";  // title of dialog
-        ofn.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT | OFN_NOCHANGEDIR;  // dialog options
+        // - OFN_NOREADONLYRETURN: The dialog will not return read-only files.
+        // - OFN_HIDEREADONLY: The read-only checkbox is hidden.
+        ofn.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT | OFN_NOCHANGEDIR
+            | OFN_NOREADONLYRETURN | OFN_HIDEREADONLY;  // dialog options
 
         // Set default extension based on the first filter's extension
         ofn.lpstrDefExt = std::strchr(filter, '\0') + 1;
+
+        // Get the current directory to set as the initial directory
+        if (GetCurrentDirectoryA(sizeof(currentDir), currentDir))
+        {
+            ofn.lpstrInitialDir = currentDir;
+        }
 
         // Display the Save dialog box
         if (GetSaveFileNameA(&ofn))
